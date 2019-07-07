85c7347ace98483dab2a5712966c99e35a96e026
João Felipe <jo_5919@hotmail.com>
2019-07-07 12:39:10 -0300

    Adds User update and auth middleware

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,13 +1,23 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import User from '../models/User';
 import AuthConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    });
+
+    if(!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validations fails'});
+    }
+
     const { email, password } = req.body;
 
-    const user = User.findOne({ where: { email }});
+    const user = await User.findOne({ where: { email }});
 
     if(!user) {
       return res.status(401).json({ error: 'User not found'});
@@ -32,4 +42,4 @@ class SessionController {
   }
 }
 
-export default SessionController;
+export default new SessionController();
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -4,15 +4,15 @@ import bcrypt from 'bcryptjs';
 class User extends Model {
   static init(sequelize) {
     super.init(
-    {
-      name: Sequelize.STRING,
-      email: Sequelize.STRING,
-      password: Sequelize.VIRTUAL,
-      password_hash: Sequelize.STRING
-    },
-    {
-      sequelize,
-    }
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING
+      },
+      {
+        sequelize,
+      }
     );
 
     this.addHook('beforeSave', async user => {
